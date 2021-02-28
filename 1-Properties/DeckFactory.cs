cabd461d3a7fdc4194817d41d3f4984c91be76bd
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-28 15:46:16 +0100

    Added properties

--- 1-Properties/DeckFactory.cs
+++ 1-Properties/DeckFactory.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Properties
 {
     using System;
@@ -13,35 +15,19 @@ namespace Properties
 
         private string[] names;
 
-        // TODO improve
-        public IList<string> GetSeeds()
+        public IList<string> Seeds
         {
-            return this.seeds.ToList();
+            get => this.seeds.ToList();
+            set => this.seeds = value.ToArray();
         }
 
-        // TODO improve
-        public void SetSeeds(IList<string> seeds)
+        public IList<string> Names
         {
-            this.seeds = seeds.ToArray();
+            get => this.names.ToList();
+            set => names = value.ToArray();
         }
 
-        // TODO improve
-        public IList<string> GetNames()
-        {
-            return this.names.ToList();
-        }
-
-        // TODO improve
-        public void SetNames(IList<string> names)
-        {
-            this.names = names.ToArray();
-        }
-
-        // TODO improve
-        public int GetDeckSize()
-        {
-            return this.names.Length * this.seeds.Length;
-        }
+        public int DeckSize => this.names.Length * this.seeds.Length;
 
         /// TODO improve
         public ISet<Card> GetDeck()
